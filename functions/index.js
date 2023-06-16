eeec410ce944766359959eea3f288a4d1420b996
Ali Murad <40003230+rxalimurad@users.noreply.github.com>
2023-06-16 22:26:17 +0500

    -- cloud function for push notifications

--- functions/index.js
+++ functions/index.js
@@ -17,36 +17,90 @@ exports.listFriendRequest = functions.firestore
   .onUpdate((change, context) => {
     const userId = context.params.userId;
     const newData = change.after.data();
+    const previousData = change.before.data();
 
+    if ((!previousData.friends && newData.friends) || (newData.friends.length > previousData.friends.length)) {
     // Retrieve the user's FCM tokens from Firestore
     return admin.firestore().collection('users').doc(userId).get()
       .then(doc => {
         const fcmTokens = doc.data().notificationTokens;
         console.log('FCM tokens found for the user.');
         console.log(fcmTokens);
-        // if (Array.isArray(fcmTokens) && fcmTokens.length > 0) {
-        //   // Send FCM notifications to all tokens
-        //   const payload = {
-        //     notification: {
-        //       title: 'Friend Request',
-        //       body: 'Your have new friend'
-        //     },
-        //     data: {
-        //       // Additional data to send with the notification (if needed)
-        //       foo: 'bar'
-        //     }
-        //   };
+        console.log(Array.isArray(fcmTokens));
+        console.log(fcmTokens);
+        if (Array.isArray(fcmTokens) && fcmTokens.length > 0) {
+          // Send FCM notifications to all tokens
+          console.log('FCM tokens is array');
+          const payload = {
+            notification: {
+              title: 'Friend Request',
+              body: 'Your have new friend request'
+            },
+            data: {
+              foo: 'bar'
+            }
+          };
 
-        //   const sendPromises = fcmTokens.map(token => sendNotification(token, payload));
-        //   return Promise.all(sendPromises);
-        // } else {
-        //   console.error('No FCM tokens found for the user.');
-        //   return null;
-        // }
+          const sendPromises = fcmTokens.map(token => sendNotification(token, payload));
+          return Promise.all(sendPromises)
+          .then(() => {
+            console.log('Notifications sent successfully.');
+          })
+          .catch(error => {
+            console.error('Error sending notifications:', error);
+          });
+        } else {
+          console.error('No FCM tokens found for the user.');
+        }
 
 
       })
       .catch(error => {
         console.error('Error fetching user FCM tokens:', error);
       });
+      
+    } else {
+        const acceptedFriendsnew = newData.friends.filter(friend => friend.status === "ACCEPTED");
+        const acceptedFriendsold = previousData.friends.filter(friend => friend.status === "ACCEPTED");
+        if (acceptedFriendsnew.length > acceptedFriendsold.length) {
+            return admin.firestore().collection('users').doc(userId).get()
+      .then(doc => {
+        const fcmTokens = doc.data().notificationTokens;
+        console.log('FCM tokens found for the user.');
+        console.log(fcmTokens);
+        console.log(Array.isArray(fcmTokens));
+        console.log(fcmTokens);
+        if (Array.isArray(fcmTokens) && fcmTokens.length > 0) {
+          // Send FCM notifications to all tokens
+          console.log('FCM tokens is array');
+          const payload = {
+            notification: {
+              title: 'Friend Request Accepted',
+              body: 'Your have new friend'
+            },
+            data: {
+              foo: 'bar'
+            }
+          };
+
+          const sendPromises = fcmTokens.map(token => sendNotification(token, payload));
+          return Promise.all(sendPromises)
+          .then(() => {
+            console.log('Notifications sent successfully.');
+          })
+          .catch(error => {
+            console.error('Error sending notifications:', error);
+          });
+        } else {
+          console.error('No FCM tokens found for the user.');
+        }
+
+
+      })
+      .catch(error => {
+        console.error('Error fetching user FCM tokens:', error);
+      });
+        }
+
+    }
   });
